613218c78f0dd6b6f1de9b66eae8b4f09f83283c
jonatan <jonatan.haaralaorosco@gmail.com>
2024-02-12 11:56:07 -0300

    added error page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import Bookmarks from './Bookmarks';
 import Explore from './Explore';
+import Error from './Error';
 import MainMenu from './MainMenu';
 import Profile from './Profile';
 
 export { 
     Bookmarks,
+    Error,
     Explore,
     Profile,
     MainMenu
--- src/routes/path.jsx
+++ src/routes/path.jsx
@@ -1,13 +1,15 @@
 import { Routes, Route } from "react-router-dom";
-import { Bookmarks, Explore, MainMenu, Profile } from '../pages'
+import { Bookmarks, Explore, Error, MainMenu, Profile } from '../pages'
+
 
 export const Path = () => {
     return (
         <Routes>
             <Route path="/" element={<MainMenu />} />
-            <Route path="bookmarks" element={<Bookmarks />} />
-            <Route path="explore" element={<Explore />} />
-            <Route path="profile" element={<Profile />} />
+            <Route path="/bookmarks" element={<Bookmarks />} />
+            <Route path="/explore" element={<Explore />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="*" element={<Error />} />
         </Routes>
     )
 }
