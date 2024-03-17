67f56cd65f3d343445bf62cf4d1acc3a22508fc6
jonatan <jonatan.haaralaorosco@gmail.com>
2024-03-17 20:09:58 -0300

    api client call functions

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,21 @@
-const api = async () => {
-  const client = await fetch('http://google.com')
-  return client.json()
+import axios from "axios";
+
+axios.defaults.baseURL = "http://localhost:3000";
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+
+
+export const getquerys = async ({ url, params }) => {
+  axios.get(`/${url}`, {
+    params
+  })
+    .then(res => { return res.data.json() })
 }
 
-export default api
+export const postquerys = async ({ url, data }) => {
+  axios.post(`/${url}`, data)
+    .then(res => {
+      console.log(res.data);
+      return res.json();
+    })
+    .catch(err => console.error(err));
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,17 @@ import Explore from './Explore';
 import Error from './Error';
 import MainMenu from './MainMenu';
 import Profile from './Profile';
+import Login from './authPages/Login';
+import SignUp from './authPages/SignUp';
+import ForgotPass from './authPages/ForgotPass';
 
-export { 
+export {
     Bookmarks,
     Error,
     Explore,
     Profile,
-    MainMenu
+    MainMenu,
+    Login,
+    SignUp,
+    ForgotPass
 }
